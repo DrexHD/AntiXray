ff83102623cb1efc267bb445f118a42565d031d5
Drex <nicknamedrex@gmail.com>
2021-08-25 10:55:24 +0200

    change: Optimize imports

--- src/main/java/me/drex/networking/mixin/ConnectionMixin.java
+++ src/main/java/me/drex/networking/mixin/ConnectionMixin.java
@@ -6,15 +6,12 @@ import io.netty.util.concurrent.GenericFutureListener;
 import me.drex.antixray.util.ClientboundLevelChunkPacketInterface;
 import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientboundLevelChunkPacket;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Queue;
 

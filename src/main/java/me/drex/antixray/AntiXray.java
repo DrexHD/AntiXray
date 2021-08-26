194dd537ef0a5363522646d1c425969881aef323
Drex <nicknamedrex@gmail.com>
2021-08-26 20:50:27 +0200

    add: Slimjar runtime dependency handling
    commit 44b9409027e5a8980cdc96db19da2967265df8c4
    Author: Drex <nicknamedrex@gmail.com>
    Date:   Thu Aug 26 20:21:39 2021 +0200
    
        fix: Slimjar logging
    
    commit e60086f55dd925f902d33d83347cfb7ae4d2e530
    Author: Drex <nicknamedrex@gmail.com>
    Date:   Thu Aug 26 20:06:14 2021 +0200
    
        fix: Slimjar support
    
    commit 58f5a7849d3261a436b7db0d872ed8c32d224852
    Author: Drex <nicknamedrex@gmail.com>
    Date:   Wed Aug 25 22:13:49 2021 +0200
    
        add: Slimjar

--- src/main/java/me/drex/antixray/AntiXray.java
+++ src/main/java/me/drex/antixray/AntiXray.java
@@ -1,5 +1,6 @@
 package me.drex.antixray;
 
+import io.github.slimjar.app.builder.ApplicationBuilder;
 import me.drex.antixray.config.Config;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
@@ -7,6 +8,10 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.MinecraftServer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.NoSuchAlgorithmException;
 
 public class AntiXray implements DedicatedServerModInitializer {
 
@@ -19,10 +24,21 @@ public class AntiXray implements DedicatedServerModInitializer {
 
     @Override
     public void onInitializeServer() {
-        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
-            minecraftServer = server;
-        });
+        downloadDependencies();
+        ServerLifecycleEvents.SERVER_STARTING.register(server -> minecraftServer = server);
         Config.loadConfig(FabricLoader.getInstance().getConfigDir().resolve("antixray.toml").toFile());
+    }
+
+    private void downloadDependencies() {
+        LOGGER.info("Downloading dependencies, this might take a couple of seconds...");
+        try {
+            ApplicationBuilder.appending("FabricAntiXray")
+                    .logger((message, args) -> LOGGER.info(message.replaceAll("\\{\\d+}", "{}"), args))
+                    .downloadDirectoryPath(FabricLoader.getInstance().getConfigDir().resolve("libraries"))
+                    .build();
+        } catch (URISyntaxException | ReflectiveOperationException | NoSuchAlgorithmException | IOException e) {
+            LOGGER.error("Failed to download dependencies:", e);
+        }
     }
 
 

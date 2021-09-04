890014d1ed398cd49b3f6f2021123b65c0e41673
DrexHD <nicknamedrex@gmail.com>
2021-09-04 22:03:46 +0200

    remove: Slimjar and Netty update

--- src/main/java/me/drex/antixray/AntiXray.java
+++ src/main/java/me/drex/antixray/AntiXray.java
@@ -1,6 +1,5 @@
 package me.drex.antixray;
 
-import io.github.slimjar.app.builder.ApplicationBuilder;
 import me.drex.antixray.config.Config;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
@@ -8,10 +7,6 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.MinecraftServer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.security.NoSuchAlgorithmException;
 
 public class AntiXray implements DedicatedServerModInitializer {
 
@@ -24,20 +19,8 @@ public class AntiXray implements DedicatedServerModInitializer {
 
     @Override
     public void onInitializeServer() {
-        downloadDependencies();
         ServerLifecycleEvents.SERVER_STARTING.register(server -> minecraftServer = server);
         Config.loadConfig(FabricLoader.getInstance().getConfigDir().resolve("antixray.toml").toFile());
     }
 
-    private void downloadDependencies() {
-        LOGGER.info("Downloading dependencies, this might take a couple of seconds...");
-        try {
-            ApplicationBuilder.appending("FabricAntiXray")
-                    .logger((message, args) -> LOGGER.info(message.replaceAll("\\{\\d+}", "{}"), args))
-                    .downloadDirectoryPath(FabricLoader.getInstance().getConfigDir().resolve("libraries"))
-                    .build();
-        } catch (URISyntaxException | ReflectiveOperationException | NoSuchAlgorithmException | IOException e) {
-            LOGGER.error("Failed to download dependencies:", e);
-        }
-    }
 }

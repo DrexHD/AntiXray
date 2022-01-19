443ddccc522026921a70d3ec9ff4ee25fd28c0ed
Drex <nicknamedrex@gmail.com>
2022-01-19 01:05:47 +0100

    Architectury and project modules (#17)
    * add: Architectury and project modules
    
    - Use Architectury
    - Add Forge mod jar
    - Minor changes
    
    * fix: Github actions

---
+++ common/src/main/java/me/drex/antixray/mixin/ServerLevelMixin.java
@@ -0,0 +1,39 @@
+package me.drex.antixray.mixin;
+
+import me.drex.antixray.interfaces.ILevel;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.progress.ChunkProgressListener;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.level.CustomSpawner;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.level.storage.LevelStorageSource;
+import net.minecraft.world.level.storage.ServerLevelData;
+import net.minecraft.world.level.storage.WritableLevelData;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.function.Supplier;
+
+@Mixin(ServerLevel.class)
+public abstract class ServerLevelMixin extends Level implements ILevel {
+    protected ServerLevelMixin(WritableLevelData writableLevelData, ResourceKey<Level> resourceKey, DimensionType dimensionType, Supplier<ProfilerFiller> supplier, boolean bl, boolean bl2, long l) {
+        super(writableLevelData, resourceKey, dimensionType, supplier, bl, bl2, l);
+    }
+
+    @Inject(
+            method = "<init>",
+            at = @At("TAIL")
+    )
+    public void onInit(MinecraftServer minecraftServer, Executor executor, LevelStorageSource.LevelStorageAccess levelStorageAccess, ServerLevelData serverLevelData, ResourceKey<Level> resourceKey, DimensionType dimensionType, ChunkProgressListener chunkProgressListener, ChunkGenerator chunkGenerator, boolean bl, long l, List<CustomSpawner> list, boolean bl2, CallbackInfo ci) {
+        initValues(executor);
+    }
+
+}
---
+++ common/src/main/java/me/drex/antixray/util/Platform.java
@@ -0,0 +1,5 @@
+package me.drex.antixray.util;
+
+public enum Platform {
+    FABRIC, FORGE;
+}

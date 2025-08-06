234e3d5aa4a210812a02f1ffed051a797cb997c9
Sebastian <sebastian97@gmail.com>
2025-08-06 03:23:26 -0700

    Make sure players can actually see the block they are hitting

--- common/src/main/java/me/drex/antixray/common/mixin/ServerPlayerGameModeMixin.java
+++ common/src/main/java/me/drex/antixray/common/mixin/ServerPlayerGameModeMixin.java
@@ -11,17 +11,41 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.phys.Vec3;
 
 @Mixin(ServerPlayerGameMode.class)
 public abstract class ServerPlayerGameModeMixin {
     @Shadow
     public ServerLevel level;
+    @Shadow
+    protected ServerPlayer player;
 
     @Inject(
-        method = "handleBlockBreakAction",
-        at = @At("TAIL")
+            method = "handleBlockBreakAction",
+            at = @At("TAIL")
     )
     public void onPlayerBreakBlock(BlockPos blockPos, ServerboundPlayerActionPacket.Action action, Direction direction, int i, int j, CallbackInfo ci) {
-        Util.getBlockController(this.level).onPlayerLeftClickBlock((ServerPlayerGameMode) (Object) this, blockPos, action, direction, i);
+        if (this.player == null) return;
+
+        var eyePos = this.player.getEyePosition(1.0F);
+        var targetVec = blockPos.getCenter();
+
+        ClipContext clipContext = new ClipContext(
+                eyePos,
+                targetVec,
+                ClipContext.Block.OUTLINE,
+                ClipContext.Fluid.NONE,
+                this.player
+        );
+
+        var rayResult = this.level.clip(clipContext);
+
+        if (rayResult == null || !rayResult.getBlockPos().equals(blockPos)) {
+            return;
+        }
+        Util.getBlockController(this.level).onPlayerLeftClickBlock((ServerPlayerGameMode)(Object)this, blockPos, action, direction, i);
     }
+
 }

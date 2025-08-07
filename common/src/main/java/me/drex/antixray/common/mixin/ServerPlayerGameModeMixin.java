4e3c8229e1ecc4f174376f5b1117456cf604a919
Sebastian <sebastian97@gmail.com>
2025-08-07 16:17:55 -0700

    Update ServerPlayerGameModeMixin.java

--- common/src/main/java/me/drex/antixray/common/mixin/ServerPlayerGameModeMixin.java
+++ common/src/main/java/me/drex/antixray/common/mixin/ServerPlayerGameModeMixin.java
@@ -11,41 +11,22 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.level.ClipContext;
-import net.minecraft.world.phys.Vec3;
 
 @Mixin(ServerPlayerGameMode.class)
 public abstract class ServerPlayerGameModeMixin {
-    @Shadow
-    public ServerLevel level;
-    @Shadow
-    protected ServerPlayer player;
+
+    @Shadow public ServerLevel level;
 
     @Inject(
             method = "handleBlockBreakAction",
-            at = @At("TAIL")
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/level/ServerLevel;destroyBlockProgress(ILnet/minecraft/core/BlockPos;I)V",
+                    ordinal = 0
+            )
     )
-    public void onPlayerBreakBlock(BlockPos blockPos, ServerboundPlayerActionPacket.Action action, Direction direction, int i, int j, CallbackInfo ci) {
-        if (this.player == null) return;
-
-        var eyePos = this.player.getEyePosition(1.0F);
-        var targetVec = blockPos.getCenter();
-
-        ClipContext clipContext = new ClipContext(
-                eyePos,
-                targetVec,
-                ClipContext.Block.OUTLINE,
-                ClipContext.Fluid.NONE,
-                this.player
+    public void onDestroyBlockProgress(BlockPos blockPos, ServerboundPlayerActionPacket.Action action, Direction direction, int i, int j, CallbackInfo ci) {
+        Util.getBlockController(this.level).onPlayerLeftClickBlock((ServerPlayerGameMode)(Object)this, blockPos, action, direction, i
         );
-
-        var rayResult = this.level.clip(clipContext);
-
-        if (rayResult == null || !rayResult.getBlockPos().equals(blockPos)) {
-            return;
-        }
-        Util.getBlockController(this.level).onPlayerLeftClickBlock((ServerPlayerGameMode)(Object)this, blockPos, action, direction, i);
     }
-
 }
